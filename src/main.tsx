7c550e2711e10c2c574e038d8a378fb339f35fcd
IC-ACADEMY <jmejia@intercon.com.mx>
2026-09-01 17:25:15 -0600

    feat: load executive demo styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,5 @@ import App from "./App";
 import "./index.css";
 import "./local.css";
 import "./v3.css";
+import "./demo.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
